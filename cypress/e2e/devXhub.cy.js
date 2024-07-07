fc9ef920bd030f96fbd1300185542bb3406e89d8
Md. Maynuddin (Mynul) <76878954+maynul1997@users.noreply.github.com>
2024-07-07 17:12:53 +0600

    Site browsing
    try to visit website

--- cypress/e2e/devXhub.cy.js
+++ cypress/e2e/devXhub.cy.js
@@ -1,7 +1,7 @@
 describe('Google Search and Gradual Scroll Test', () => {
   it('should perform a search and scroll through the results gradually', () => {
     // Visit the Google homepage
-    cy.visit('https://www.google.com');
+    cy.visit('https://devxhub.com/');
 
     // Accept cookies if the consent form appears (optional, based on your location)
     cy.get('button').then(($btn) => {
@@ -15,3 +15,13 @@ describe('Google Search and Gradual Scroll Test', () => {
 
     // Click the Google search button by its text
     cy.contains('input', 'Google Search').click();
+
+    // Wait for the search results to appear
+    cy.get('#search').should('be.visible');
+
+    // Verify that the search results contain the expected text
+    cy.get('#search').contains('Cypress.io').should('be.visible');
+
+   
+    });
+  });

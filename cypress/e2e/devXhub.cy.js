059f302d50daa2c68a279dbb62c0cfe6cd0fb915
Md. Maynuddin (Mynul) <76878954+maynul1997@users.noreply.github.com>
2024-07-07 17:38:37 +0600

    scroll up done
    scroll up done successfully

--- cypress/e2e/devXhub.cy.js
+++ cypress/e2e/devXhub.cy.js
@@ -10,13 +10,18 @@ describe('Google Search and Gradual Scroll Test', () => {
       }
     });
 
-     //need to scroll page
-     // Scroll down
-     for (let i = 0; i < 20; i++) {
-      cy.scrollTo(0, 900 * (i + 1));
-      cy.wait(500);
-  }
+        //need to scroll page
+        // Scroll down
+        for (let i = 0; i < 20; i++) {
+          cy.scrollTo(0, 900 * (i + 1));
+          cy.wait(500);
+        }
 
+        // Scroll up
+        for (let i = 0; i < 20; i++) {
+          cy.scrollTo(0, 900 * (20 - i - 1));
+          cy.wait(500);
+        }
 
    
     });
